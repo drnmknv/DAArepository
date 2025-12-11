22c125322928d6aecaabe49864a0f13c64726fea
drn <darynmukanov41@gmail.com>
2025-12-11 09:02:06 +0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.Scanner;
+
+class oop {
+    void main() {
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for (int i = 0 ; i < n ; i++) {
+            System.out.println("Hello World!");
+        }
     }
 }
